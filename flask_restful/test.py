e9e24eae0105a4270a92563a3447f9588b897abe
Jimin Gao <jimin.gao@nexusguard.com>
2020-10-09 16:37:34 +0800

    update restful example

--- flask_restful/test.py
+++ flask_restful/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 from flask_restful import Resource, Api
 
 
@@ -25,6 +25,90 @@ api.add_resource(Multi, '/multi/<int:num>')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return "test"
+
+
+
+####### #other methods ######
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+#Get all tasks
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+
+    return jsonify({'tasks': tasks})
+
+#Get One by task id
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+# @app.errorhandler(400)
+# def bad_req(error):
+#    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+####POST Methods ####
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+##PUT Update ###
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != str:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+####Delete ##
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host="0.0.0.0")
--- flask_restful/test.sh
+++ flask_restful/test.sh
@@ -6,3 +6,17 @@ curl -i -H "Content-Type: application/json" -X POST  http://localhost:80/abc  -d
 curl -i -X POST http://localhost:80/abc  -d '{"abc": "10"}'
 
 curl -i http://localhost:80/multi/3
+
+
+curl -i http://localhost:80/todo/api/v1.0/tasks
+curl -i http://localhost:80/todo/api/v1.0/tasks/2
+curl -i http://localhost:80/todo/api/v1.0/tasks/112
+
+
+curl -i -H "Content-Type: application/json" -X POST http://localhost:80/todo/api/v1.0/tasks -d '{"title": "test", "description": "just for test"}'
+
+
+curl -i -H "Content-Type: application/json" -X PUT http://localhost:80/todo/api/v1.0/tasks/3 -d '{"title": "test2", "description": "just for test2", "done": true }'
+
+
+curl -i -X DELETE http://localhost:80/todo/api/v1.0/tasks/3 
